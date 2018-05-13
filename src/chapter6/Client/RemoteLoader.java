8182dae7a6c892d594e01f1b54206d25dadf0eaf
Akanksha Gupta <14840870+akangupt@users.noreply.github.com>
2018-05-13 11:21:58 +0530

    Making remote control with multiple slots

--- src/chapter6/Client/RemoteLoader.java
+++ src/chapter6/Client/RemoteLoader.java
@@ -1,18 +1,49 @@
 package chapter6.Client;
 
+import chapter6.Command.CeilingFanHighCommand;
+import chapter6.Command.CeilingFanOffCommand;
+import chapter6.Command.LightOffCommand;
 import chapter6.Command.LightOnCommand;
+import chapter6.Command.StereoOffCommand;
+import chapter6.Command.StereoOnWithCDCommand;
 import chapter6.Invoker.RemoteControl;
 import chapter6.Receiver.CeilingFan;
 import chapter6.Receiver.Light;
+import chapter6.Receiver.Stereo;
 
 public class RemoteLoader {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         RemoteControl remote = new RemoteControl();
 
         Light livingRoomLight = new Light("Living room");
-        LightOnCommand lightOnCommand = new LightOnCommand(livingRoomLight);
+        Light kitchenLight = new Light("Kitchen");
+        CeilingFan ceilingFan = new CeilingFan("Living room");
+        Stereo stereo = new Stereo("Living Room");
 
-        remote.setCommand(lightOnCommand);
-        remote.buttonWasPressed();
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+
+        CeilingFanHighCommand livingRoomCeilingFanOn = new CeilingFanHighCommand(ceilingFan);
+        CeilingFanOffCommand livingRoomCeilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+        StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+
+        remote.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        remote.setCommand(1, kitchenLightOn, kitchenLightOff);
+        remote.setCommand(2, livingRoomCeilingFanOn, livingRoomCeilingFanOff);
+        remote.setCommand(3, stereoOnWithCD, stereoOff);
+
+        remote.onButtonWasPressed(0);
+        remote.onButtonWasPressed(1);
+        remote.onButtonWasPressed(2);
+        remote.onButtonWasPressed(3);
+
+        remote.offButtonWasPressed(0);
+        remote.offButtonWasPressed(1);
+        remote.offButtonWasPressed(2);
+        remote.offButtonWasPressed(3);
     }
 }
--- src/chapter6/Invoker/RemoteControl.java
+++ src/chapter6/Invoker/RemoteControl.java
@@ -3,16 +3,24 @@ package chapter6.Invoker;
 import chapter6.Command.Command;
 
 public class RemoteControl {
-    private Command slot;
+    private Command[] onCommands;
+    private Command[] offCommands;
 
     public RemoteControl() {
+        onCommands = new Command[7];
+        offCommands = new Command[7];
     }
 
-    public void setCommand(Command command) {
-        slot = command;
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
     }
 
-    public void buttonWasPressed() {
-        slot.execute();
+    public void onButtonWasPressed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPressed(int slot) {
+        offCommands[slot].execute();
     }
 }
